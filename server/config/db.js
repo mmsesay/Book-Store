ba8dbf801d8030ae4815f0c3eccce753842b5909
Muhammad Sesay <maej_01@hotmail.com>
2020-04-10 06:13:37 -0700

    add: reading data from local mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 // export the db
 module.exports = {
-    MongoURI: 'mongodb+srv://maej:maej123@cluster0-qssks.mongodb.net/test?retryWrites=true&w=majority'
+    // MongoURI: 'mongodb+srv://maej:maej123@cluster0-qssks.mongodb.net/test?retryWrites=true&w=majority'
+    MongoURI: 'mongodb://localhost/bookStore'
 }
